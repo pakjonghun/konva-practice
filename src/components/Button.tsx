96ffb6374dd1dce45931d09aa5ca4117f48d6ffb
jonghunpak <fireking5997@gmail.co.kr>
2024-09-30 02:06:01 +0900

    서버키트 api 인터페이스 작성

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,4 @@
-import { useCallback, useState } from 'react';
-import useDebounce from '../hooks/useDebounce';
+import { upload } from '../hooks/useNodeData';
 
 const Button = () => {
   // const setCount = useBoardStore((state) => state.setCount);
@@ -21,7 +20,44 @@ const Button = () => {
   return (
     <div>
       <input
-        className="border bg-gray-100"
+        onChange={(e) => {
+          const file = e.target.files?.[0];
+          if (!file) return;
+          const formBody = new FormData();
+          formBody.append('avatar', file);
+          formBody.append('thumbnail', file);
+          const data = {
+            name: '테스터',
+            phone: '010-1234-1351',
+            birth: '1999-10-10',
+            gender: 'M',
+            address: 'seoul',
+            programId: 'fbae8954-1a65-4f66-b253-522b4f5effd1',
+            protectorList: [
+              {
+                name: '박수지',
+                phone: '010-1313-4242',
+                relation: '아들',
+              },
+              {
+                name: '박수고',
+                phone: '010-2313-4242',
+                relation: '친구',
+              },
+            ],
+            description: ['조심해요', '조심해요2'],
+          };
+          formBody.append('data', JSON.stringify(data));
+          upload(formBody)
+            .then(() => {
+              e.target.value = '';
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }}
+        type="file"
+        className="border-2 border-red-100  bg-pink-300"
         value={''}
         // onChange={(event) => handleChangeKeyword(event.target.value)}
       />
--- src/hooks/useNodeData.ts
+++ src/hooks/useNodeData.ts
@@ -16,3 +16,16 @@ export const useLoadNodeData = () => {
     queryFn: loadNodeData,
   });
 };
+
+export const upload = (file: FormData) => {
+  return instance
+    .post('https://api.server-kit.com/run/check-in', file, {
+      headers: {
+        serverkit_instance_id: '66f12f07f37f857fefb0a0c3',
+        userkit_token:
+          'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InhaNzBTUzl4NE5zTVNuZ3ciLCJwZXJtaXNzaW9ucyI6WyI2NmYxMmYwN2YzN2Y4NTdmZWZiMGEwYzNfNjZmM2RhYWU5YjA1ZjhlYmI5ODVhM2NiIl0sInVzZXJHcm91cElkIjoiNjZmMTJmMDdmMzdmODU3ZmVmYjBhMGMzXzY2ZjNkYWFlOWIwNWY4ZWJiOTg1YTNjYSIsImluc3RhbmNlSWQiOiI2NmYxMmYwN2YzN2Y4NTdmZWZiMGEwYzMiLCJpYXQiOjE3MjczMzQzNjF9.S19i8YxKPiVt9r8VqupKBjOIqrsI7gfJtIaT1ilwnMO5dt3z55L4bUWTEevtBGgMl3XjVW8fiJiH0qRDKK9QvY9itSXMCOWxSlQhqv2PrfZzRMrDKJ7muyi8Xl5-HBycVB8wRRiCrsYvqeAHHNESBl1COfKTdJXZm_sGLMhyCtgcxxrBlF6nA2aALAmueX4HDx-flnGebX5F0lzt7iH-2oLksZMramn3yK3XUc1LcBOpOvSEQBy0qPeYw8Sa0222lnK2i0__LxnWAc8fVpraPnV4n0W3CdWllK2e1su27mDLmD0sc2_uweRo95LY4R2pSJvBEh5f0sjPxRZu4PxpPg',
+        serverkit_project_id: '66f12f0760f82da8f7b94be7',
+      },
+    })
+    .then((res) => res.data);
+};
